7447615885c525d4f93c48059b3f6f08ff12de6f
MartinaMagdi <martinmagdi6@gmail.com>
2023-08-13 23:07:20 +0300

    Showing the winner name in the winning page

--- Scripts/winning.js
+++ Scripts/winning.js
@@ -1,10 +1,11 @@
-var isWin = true;
 var containerDiv = document.getElementsByClassName("container");
 var winningStatusDiv = document.getElementById("winning-status");
+var winnerName = getCookie("winner");
+console.log(winnerName);
 
-if (isWin) {
+if (winnerName) {
   containerDiv[0].classList.add("winning");
-  winningStatusDiv.innerHTML = `The winner is <br> <span class='winner-name'>Martina</span`;
+  winningStatusDiv.innerHTML = `The winner is <br> <span class='winner-name'>${winnerName}</span`;
 } else {
   winningStatusDiv.innerHTML = `No one is winner`;
 }
